1c97cb3be19b488624a5366ae1e03129b280c4a3
KIPROTICH BRIAN <bryannkoech7@gmail.com>
2020-11-04 12:58:16 +0300

    payment summary layout done

---
+++ OvalBank/app/src/main/java/com/example/ovalbank/PaymentSummary.java
@@ -0,0 +1,14 @@
+package com.example.ovalbank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PaymentSummary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_summary);
+    }
+}
