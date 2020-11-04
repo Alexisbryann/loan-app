6051746113bea7ec81c0c31a90172394bb2cfb21
KIPROTICH BRIAN <bryannkoech7@gmail.com>
2020-11-04 14:40:27 +0300

    OtherPayBills layout done

---
+++ OvalBank/app/src/main/java/com/example/ovalbank/BuyGoodsAndServices.java
@@ -0,0 +1,14 @@
+package com.example.ovalbank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BuyGoodsAndServices extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buy_goods_and_services);
+    }
+}
---
+++ OvalBank/app/src/main/java/com/example/ovalbank/OtherPayBills.java
@@ -0,0 +1,14 @@
+package com.example.ovalbank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OtherPayBills extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_other_pay_bills);
+    }
+}
