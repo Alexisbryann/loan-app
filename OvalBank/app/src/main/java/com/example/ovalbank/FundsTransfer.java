05bd3b2e73a41f4583b73010af8cb975871a41fd
KIPROTICH BRIAN <bryannkoech7@gmail.com>
2020-11-04 15:11:07 +0300

    FundsTransfer layout done

---
+++ OvalBank/app/src/main/java/com/example/ovalbank/FundsTransfer.java
@@ -0,0 +1,14 @@
+package com.example.ovalbank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FundsTransfer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_funds_transfer);
+    }
+}
