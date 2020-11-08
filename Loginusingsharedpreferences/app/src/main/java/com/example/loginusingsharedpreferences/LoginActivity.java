d68373f1303e30d9e496a7f61b81cbe05ab502ac
KIPROTICH BRIAN <bryannkoech7@gmail.com>
2020-11-08 16:24:49 +0300

    Adding search bar

---
+++ Loginusingsharedpreferences/app/src/main/java/com/example/loginusingsharedpreferences/LoginActivity.java
@@ -0,0 +1,79 @@
+package com.example.loginusingsharedpreferences;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText mUsername;
+    private EditText mPassword;
+    private Button mLogin;
+    private TextView mRegister;
+    private String mUname;
+    private String mPword;
+
+    public static  String MyPreferences = "myPrefs";
+    private SharedPreferences mSharedPreferences;
+    private final String USERNAME = "username";
+    private final String KEY_PASSWORD = "password";
+    private String mUname1;
+    private String mPword1;
+    private String mUname2;
+    private String mPword2;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activtiy);
+
+        mUsername = findViewById(R.id.edt_username);
+        mPassword = findViewById(R.id.edt_password);
+        mLogin = findViewById(R.id.button_login);
+        mRegister = findViewById(R.id.tv_register);
+
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               validateUser();
+            }
+        });
+    }
+    public void validateUser(){
+
+        mSharedPreferences = getSharedPreferences(MyPreferences,MODE_PRIVATE);
+
+        mUname1 = mUsername.getText().toString();
+        mPword1 = mPassword.getText().toString();
+
+        mUname2 = mSharedPreferences.getString("Username","");
+        mPword2 = mSharedPreferences.getString("Password","");
+
+        if (mUname1.equals(mUname2) && mPword1.equals(mPword2)&&!mUname2.isEmpty()&&!mPword2.isEmpty())
+        {
+            Toast.makeText(LoginActivity.this,"Login Successful",Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            finish();
+            startActivity(intent);
+        }
+        if (mUname2.isEmpty()||!mUname1.matches(mUname2)){mUsername.setError("User does not exist");}
+        if (mUname1.isEmpty()){mUsername.setError("Username can not be empty");}
+        if (mPword1.isEmpty()){mPassword.setError("Password can not be empty");}
+    }
+}
